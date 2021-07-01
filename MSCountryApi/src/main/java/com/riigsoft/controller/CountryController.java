a89c9a69c201c54dcf119a3432df71b420ea796f
Abdallah <abdamoh1@gmail.com>
2021-07-01 19:57:17 +0530

    add one api with param continent

--- MSCountryApi/src/main/java/com/riigsoft/controller/CountryController.java
+++ MSCountryApi/src/main/java/com/riigsoft/controller/CountryController.java
@@ -3,18 +3,20 @@ package com.riigsoft.controller;
 import com.riigsoft.model.Country;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/countries/api/v1")
 public class CountryController {
 
     @GetMapping("/all")
-    public ResponseEntity<List<Country>> getAll() {
+    public ResponseEntity<?> getAll() {
         //We can use RestTemplate to consume api
             //String url ="https://restcountries.eu/rest/v2/all";
 
@@ -29,5 +31,31 @@ public class CountryController {
                 )
         );
 
+    }
+
+    @GetMapping("/find/{continent}")
+    public ResponseEntity<List<Country>>
+    findByContinent(@PathVariable String continent){
+        String continentLC = continent.toLowerCase();
+        if(continentLC.equals("asia")) {
+            return ResponseEntity.ok(
+                    List.of(
+                            new Country("INDIA", "RUPEE"),
+                            new Country("PAKISTAN", "RUPEE")
+
+                    )
+            );
+        }
+            if(continentLC.equals("africa")){
+            return ResponseEntity.ok(
+                     List.of(
+                        new Country("SOMALIA","SHLN"),
+                        new Country("ETHOIPIA","BIRR")
+
+                    )
+            );
+        }
+
+        return ResponseEntity.ok(List.of());
     }
 }
